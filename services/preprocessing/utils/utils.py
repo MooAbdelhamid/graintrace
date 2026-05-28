22916816381be09080d91603625730ab6801eb40
MooAbdelhamid <moethedataanalyst@gmail.com>
2026-05-28 13:16:58 +0200

    added comments to utils

--- services/preprocessing/utils/utils.py
+++ services/preprocessing/utils/utils.py
@@ -3,11 +3,17 @@ import numpy as np
 
 
 def decode_image(image_bytes: bytes) -> np.ndarray:
+    """
+    Converts from bytes to np
+    """
     arr = np.frombuffer(image_bytes, np.uint8)
     image = cv2.imdecode(arr, cv2.IMREAD_COLOR)
     return image
 
 
 def encode_image(image: np.ndarray, ext=".png") -> bytes:
+    """
+    Converts from np to bytes
+    """
     _, buf = cv2.imencode(ext, image)
     return buf.tobytes()
